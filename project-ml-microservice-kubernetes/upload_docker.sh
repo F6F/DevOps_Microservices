d69e8d2d3a89ba66e751df0366512119bc1358d4
Tobias Hobmeier <tobias@antifuse.de>
2022-01-08 21:00:06 +0100

    update upload script

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -6,10 +6,13 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+#dockerpath=f66f/outputprediction1
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
 
+
 # Step 3:
 # Push image to a docker repository
+docker push f66f/udacity:latest
