13f244bac70ca1785d67ae35fdb15ad4200f7f03
Tobias Hobmeier <tobias@antifuse.de>
2022-01-08 20:59:53 +0100

    add tag

--- project-ml-microservice-kubernetes/run_docker.sh
+++ project-ml-microservice-kubernetes/run_docker.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
+tag=f66f/udacity
+
 ## Complete the following steps to get Docker running locally
 
 # Step 1:
 # Build image and add a descriptive tag
-docker build . --tag=$1
+docker build --tag=$tag .
 
 # Step 2: 
 # List docker images
@@ -12,4 +14,4 @@ docker image ls
 
 # Step 3: 
 # Run flask app
-docker run -p 8000:80 -it $1 bash 
+docker run -p 8000:80 -it $tag:latest bash 
