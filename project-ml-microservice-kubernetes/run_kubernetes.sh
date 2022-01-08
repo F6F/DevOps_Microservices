47fb11b7a610e3fa61bb91557b929033f59531e6
Tobias Hobmeier <tobias@antifuse.de>
2022-01-08 21:32:29 +0100

    run kubernets update

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,15 +4,20 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=f66f/udacity:latest
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+#docker run -p 8000:80 -it $tag:latest bash 
+kubectl create deployment --image=f66f/udacity:latest uda-app
+kubectl set env deployment/uda-app  DOMAIN=cluster
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods --all-namespaces 
 
 # Step 4:
 # Forward the container port to a host
-
+#kubectl expose deployment uda-app --port=8000 --target-port=80 --name=uda-app
+#kubectl expose deployment uda-app --type=NodePort --name=uda-node --port=8000 --target-port=80
+kubectl port-forward deployment/uda-app 8000:80
